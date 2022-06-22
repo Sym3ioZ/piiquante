98f9334f6e7d670125f742db276340a7649a3c74
Sym3ioZ <tortevois.benoit@gmail.com>
2022-06-22 11:27:53 +0200

    user routes OK, auth and multer OK, sauces routes 70%

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require ('./models/user');
-const Sauce = require ('./models/sauce');
-const cors = require('cors');
-const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const multer = require('multer');
+const cors = require('cors');
+const path = require('path');
+
+const User = require ('./models/user');
+const Sauce = require ('./models/sauce');
+
+const auth = require ('./middleware/auth');
+const multer = require('./middleware/multer-config');
+
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -16,6 +22,9 @@ mongoose.connect('mongodb+srv://Sym3ioZ:Moikoisen72!@cluster0.5acqt.mongodb.net/
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Declaring static path to images
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Route signup
 app.post('/api/auth/signup', (req, res, next) => {
@@ -37,18 +46,20 @@ app.post('/api/auth/login', (req,res, next) => {
   User.findOne({ email: req.body.email })
     .then (user => {
       if (!user) {
-        return res.status(401).json({ error });
+        return res.status(401).json({ error: 'Utilisateur non trouvé' });
       }
       bcrypt.compare(req.body.password, user.password)
         .then (valid => {
+          console.log(valid);
           if (!valid) {
-            return res.status(401).json({ error });
+            console.log(!valid);
+            return res.status(401).json({ error: 'Mot de passe invalide' });
           }
           res.status(200).json({
             userId: user._id,
             token: jwt.sign(
               { userId: user._id},
-              'RANDOM-SECRET-TOKEN',
+              'RANDOM_SECRET_TOKEN',
               { expiresIn: '24h'}
             )
           });
@@ -59,16 +70,30 @@ app.post('/api/auth/login', (req,res, next) => {
 });
 
 // Route sauces
-app.post('/api/sauces', (req, res, next) => {
-  console.log(req);
-  const json = req.body.sauce;
-  console.log(json);
-  return res.status(400).json({ message: 'error' });
+app.post('/api/sauces', auth, multer, (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauce ({
+    userId: sauceObject.userId,
+    name: sauceObject.name,
+    manufacturer: sauceObject.manufacturer,
+    description: sauceObject.description,
+    mainPepper: sauceObject.mainPepper,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    heat: sauceObject.heat,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  })
+  sauce.save()
+    .then (() => res.status(201).json({ message: 'Sauce enregistrée avec succès !'}))
+    .catch (error => res.status(400).json({ error }));
 })
 
-app.get('/api/sauces', (req, res, next) => {
-  console.log('get réussi !');
-
-})
+app.get('/api/sauces', auth, (req, res, next) => {
+  Sauce.find()
+    .then (sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
